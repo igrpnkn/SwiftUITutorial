eb61f0e7c2cc4436b2b7b695de26ba31f5e9ca78
Igor Penkin <penkin.igor@gmail.com>
2024-11-23 20:27:34 +0300

    Empty project

---
+++ SwiftUITutorial/SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Igor Penkin on 23.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
