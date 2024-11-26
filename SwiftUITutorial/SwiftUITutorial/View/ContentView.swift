6f64530e884b6382077277e4227834cc3c561b8c
Igor Penkin <penkin.igor@gmail.com>
2024-11-27 01:40:51 +0300

    Section 6: Set up navigation between list and detail

--- SwiftUITutorial/SwiftUITutorial/View/ContentView.swift
+++ SwiftUITutorial/SwiftUITutorial/View/ContentView.swift
@@ -12,34 +12,7 @@ struct ContentView: View {
     private let imageOffset: CGFloat = 130
 
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: mapHeight)
-            CircleImage()
-                .offset(y: -imageOffset)
-                .padding(.bottom, -imageOffset)
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                    .foregroundStyle(.black)
-                HStack {
-                    Text("Joshua Tree National Park")
-                    Spacer()
-                    Text("California")
-                }
-                    .font(.subheadline)
-                    .foregroundStyle(.black)
-                    .padding(.bottom)
-                Divider()
-                Text("About Turtle Rock")
-                    .font(.title2)
-                    .padding(.top)
-                Text("Descriptive text goes here...")
-                    .font(.subheadline)
-            }
-                .padding()
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
---
+++ SwiftUITutorial/SwiftUITutorial/View/LandmarkDetail.swift
@@ -0,0 +1,48 @@
+//
+//  LandmarkDetail.swift
+//  SwiftUITutorial
+//
+//  Created by Igor Penkin on 27.11.2024.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    private let mapHeight: CGFloat = 300
+    private let imageOffset: CGFloat = 130
+    
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: mapHeight)
+            CircleImage()
+                .offset(y: -imageOffset)
+                .padding(.bottom, -imageOffset)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundStyle(.black)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                    .font(.subheadline)
+                    .foregroundStyle(.black)
+                    .padding(.bottom)
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                    .padding(.top)
+                Text("Descriptive text goes here...")
+                    .font(.subheadline)
+            }
+                .padding()
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
