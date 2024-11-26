c5f426e17bc32b07929adc66cc23c9a0a65c86ba
Igor Penkin <penkin.igor@gmail.com>
2024-11-27 01:13:05 +0300

    Section 2: Create the row view

---
+++ SwiftUITutorial/SwiftUITutorial/View/LandmarkRow.swift
@@ -0,0 +1,26 @@
+//
+//  LandmarkRow.swift
+//  SwiftUITutorial
+//
+//  Created by Igor Penkin on 27.11.2024.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks.first!)
+}
