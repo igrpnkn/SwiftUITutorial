8b5d8cbbd20fdcd81b77e73511003effc8292236
Igor Penkin <penkin.igor@gmail.com>
2024-11-27 01:05:30 +0300

    Section 1: Create a landmark model

---
+++ SwiftUITutorial/SwiftUITutorial/Model/Landmark.swift
@@ -0,0 +1,39 @@
+//
+//  Landmark.swift
+//  SwiftUITutorial
+//
+//  Created by Igor Penkin on 27.11.2024.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+
+    private var imageName: String
+    private var coordinates: Coordinates
+}
+
+extension Landmark {
+    var image: Image {
+        Image(imageName)
+    }
+
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
