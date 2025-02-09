3760b6c7922026851eef050502f57d89fe3e69be
Igor Penkin <penkin.igor@gmail.com>
2025-02-09 15:36:40 +0300

    Section 1: Add a watchOS target

---
+++ SwiftUITutorial/WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Igor Penkin on 09.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
