423f7d75b1fe706463d57964e4abd9a2675a8e0d
Igor Penkin <penkin.igor@gmail.com>
2025-02-10 08:44:46 +0300

    Section 2: Create a macOS detail view

--- SwiftUITutorial/MacLandmarks/ContentView.swift
+++ SwiftUITutorial/MacLandmarks/ContentView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-//    The preview no longer builds because the LandmarkList uses LandmarkDetail, but you haven’t defined a detail view for the macOS app yet. You’ll take care of that in the next section.
     var body: some View {
         LandmarkList()
             .frame(minWidth: 700, minHeight: 300)
